1bacc51a3734885e0ca383bb83a51b59eb154cfd
Lendon Ato <lendon.n.ato@gmail.com>
2024-02-22 00:53:05 +0800

    Revision 3, 11-15

--- thesis/views.py
+++ thesis/views.py
@@ -4,8 +4,14 @@ from django.template import loader
 from django.shortcuts import render
 import datetime
 today = datetime.date.today()
-print(today)
 
+# TODO
+# change color if fake or real
+# FAKE = 9A01014A
+# Real = 019A5A4A
+# Default =  4E5DDD4A
+# if else in here 
+radialColor="#019A5A"
 
 def index(request):
     template = loader.get_template('index.html')
@@ -17,10 +23,9 @@ def startPage(request):
 
 def versionTwo(request):
     template = loader.get_template('views/start_version2.html')
-    return HttpResponse(template.render())
+    return HttpResponse(template.render({'radialColor': radialColor }))
 
 def aboutPage(request):
-    
     template = loader.get_template('views/about_page.html')
     return HttpResponse(template.render({'year': today.strftime('%G')}))
    
