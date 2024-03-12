b5c2ebd4b7015df15b8afecc8a446dc6432d05a1
Lendon Ato <lendon.n.ato@gmail.com>
2024-03-13 06:58:53 +0800

    First Simulator

--- manage.py
+++ manage.py
@@ -2,6 +2,34 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import string
+
+from sklearn.base import BaseEstimator, TransformerMixin
+import unicodedata
+import re
+
+class TextPreprocessor(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return [self.preprocess(text) for text in X]
+
+    def preprocess(self, text):
+        # print(textes)
+        text = str(text)
+        text = text.lower()
+        normalized = unicodedata.normalize("NFD", text)
+        text = "".join(c for c in normalized if unicodedata.category(c) != "Mn")
+        text = re.sub(r'\[.*?\]', '', text)
+        text = re.sub(r"\\W", " ", text)
+        text = re.sub(r'https?://\S+|www\.\S+', '', text)
+        text = re.sub(r'<.*?>+', '', text)
+        text = re.sub(r'[%s]' % re.escape(string.punctuation), '', text)
+        text = re.sub(r'\n', '', text)
+        text = re.sub(r'\w*\d\w*', '', text)
+        return text
+
 
 
 def main():
--- thesis/views.py
+++ thesis/views.py
@@ -4,12 +4,23 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import NameForm 
 
+import numpy as np
 import datetime
 today = datetime.date.today()
 
 import joblib
 
-loaded_stack_model = joblib.load("static/joblib/stack_lr.joblib")
+
+best_estimator = joblib.load("static/models/best_estimator.joblib")
+stack_xgboost = joblib.load("static/models/stack_xgboost.joblib")
+
+
+base_knn = joblib.load('static/models/base/baseline_knn.joblib')
+base_lr = joblib.load('static/models/base/baseline_lr.joblib')
+base_nb = joblib.load('static/models/base/baseline_nb.joblib')
+base_rf = joblib.load('static/models/base/baseline_rf.joblib')
+base_svm = joblib.load('static/models/base/baseline_svm.joblib')
+
 # print(loaded_stack_model)
 # TODO
 # change color if fake or real
@@ -17,15 +28,18 @@ loaded_stack_model = joblib.load("static/joblib/stack_lr.joblib")
 # Real = 019A5A4A
 # Default =  4E5DDD4A
 # if else in here 
-radialColor="#4E5DDD4A"
+
 
 def factCheck(text):
-    factChecked = text
+    return stack_xgboost.predict(text.split(' ',0))
 
-    return factChecked
+
 
 def versionTwo(request):
      # if this is a POST request we need to process the form data
+    
+    toDisplay = 1
+    radialColor="#4E5DDD4A"
     if request.method == "POST":
         # create a form instance and populate it with data from the request:
         form = NameForm(request.POST)
@@ -36,18 +50,26 @@ def versionTwo(request):
             # if else
             # redirect to a new URL:
             result = factCheck(form.cleaned_data['content'])
-            print(result)
-            
-            return render(request, 'views/start_version2.html' , {"form": form, 'radialColor': "#019A5A4A" ,"todisplay": 3 , 'percentage': 20 })
+            real = np.count_nonzero(result == 1)
+            fake = np.count_nonzero(result == 0)
+            print("real: " +' '+ str(real))
+            print("fake: " +' '+ str(fake))
+            if fake > real :
+                toDisplay = 4
+                radialColor = "#9A01014A"
+            else:
+                toDisplay = 3
+                radialColor = '#019A5A4A'
+
+            return render(request, 'views/start_version2.html' , {"form": form, 'radialColor': radialColor ,"todisplay": toDisplay , 'percentage': 100 })
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = NameForm()
-
-    template = loader.get_template('views/start_version2.html')
-    return render(request, 'views/start_version2.html' , {"form": form, 'radialColor': radialColor, 'todisplay': 1, 'percentage': 75 })
     
-  
+    return render(request, 'views/start_version2.html' , {"form": form, 'radialColor': radialColor, 'todisplay': toDisplay, 'percentage': 75 })
+    
+
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
