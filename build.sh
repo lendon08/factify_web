5c0326653f6b1bb3787e8fcaae288f87bcf54f71
Lendon Ato <lendon.n.ato@gmail.com>
2024-04-02 23:40:59 +0800

    update

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+pip install pypiwin32
+
 # Convert static asset files
 python manage.py collectstatic --no-input
 
